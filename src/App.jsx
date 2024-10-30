36e36ce7ed30d78c1b4e1ac8676c3bf6dbeea2d4
Veridiana Lucena <veridianalucena@gmail.com>
2024-10-30 20:54:03 -0300

    Add imports and content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
+import { Header } from './components/Header'
+import { Post } from './Post'
+
+import './global.css'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <div>
+      <Header />
+      <Post
+        author="Veridiana"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, accusamus!" />
+      <Post
+        author="Lucas"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio odio minima est ea." />
+    </div>
   )
 }
 
