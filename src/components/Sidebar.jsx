778ac06d63400731e56e4932eaa819665edb011c
Veridiana Lucena <veridianalucena@gmail.com>
2024-11-04 20:12:13 -0300

    Added Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -9,12 +11,15 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img className={styles.avatar}
+          src='https://www.github.com/haaveeni.png' />
+
         <strong>Veridiana Lucena</strong>
         <span>FrontEnd Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">< PencilLine size={20} />Editar seu perfil</a>
       </footer>
     </aside>
   )
