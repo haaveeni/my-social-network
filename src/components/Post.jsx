d7c7a8742c985922aec2f1243002da9bb56575a3
Veridiana Lucena <veridianalucena@gmail.com>
2024-12-17 09:44:52 -0300

    Remove boolean after applying destructuring to props of Avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,7 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar hasBorder src='https://github.com/haaveeni.png' />
+          <Avatar src='https://github.com/haaveeni.png' />
           <div className={styles.authorInfo}>
             <strong>Veridiana Lucena</strong>
             <span>FrontEnd Developer</span>
