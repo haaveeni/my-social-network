7f05247e727443a75a0a6bc93a132f1f6c001528
Veridiana Lucena <veridianalucena@gmail.com>
2024-12-16 22:09:13 -0300

    Add Avatar component

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,9 @@
+import styles from './avatar.module.css'
+
+export function Avatar(props) {
+  return (
+    <img className={props.hasBorder ? styles.avatarWithBorder : styles.avatar}
+      src={props.src}
+    />
+  )
+}
