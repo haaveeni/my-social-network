4c1079c677d83a56d460db6f7dd8c05fd5e64c08
Veridiana Lucena <veridianalucena@gmail.com>
2025-02-15 16:15:43 -0300

    Add props, destructuring and iteration with map

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,36 @@ import styles from './App.module.css'
 
 import './global.css'
 
+const posts = [
+  {
+    author: {
+      avatarUrl: "https://github.com/haaveeni.png",
+      name: "Veridiana Lucena",
+      role: "FrontEnd Developer"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraaaaaa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'paragraph', content: <a href=''>jane.design/doctorcare</a> },
+    ],
+    publishedAt: new Date('2025-01-30 20:00:00'),
+  },
+  {
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO Rocketseat"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraaaaaa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'paragraph', content: <a href=''>jane.design/doctorcare</a> },
+    ],
+    publishedAt: new Date('2025-01-30 20:00:00')
+  }
+
+]
+
 export function App() {
 
   return (
@@ -15,9 +45,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
-
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
 
